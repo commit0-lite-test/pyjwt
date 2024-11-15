017b3619ef805e68f944872357058b18dfdbbd1c
nanjiangwill <willjiang2018@gmail.com>
2024-11-15 13:11:34 -0800

    fix: Update cryptography version handling in bug report information

--- jwt/help.py
+++ jwt/help.py
@@ -3,15 +3,17 @@ import platform
 import sys
 from typing import Dict
 from . import __version__ as pyjwt_version
+
 try:
     import cryptography
+
     cryptography_version = cryptography.__version__
 except ModuleNotFoundError:
-    cryptography_version = ''
+    cryptography_version = ""
+
 
 def info() -> Dict[str, Dict[str, str]]:
-    """
-    Generate information for a bug report.
+    """Generate information for a bug report.
     Based on the requests package help utility module.
     """
     return {
@@ -31,11 +33,14 @@ def info() -> Dict[str, Dict[str, str]]:
         "system": {
             "python": sys.version,
             "executable": sys.executable,
-        }
+        },
     }
+
 
 def main() -> None:
     """Pretty-print the bug information as JSON."""
     print(json.dumps(info(), sort_keys=True, indent=2))
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
